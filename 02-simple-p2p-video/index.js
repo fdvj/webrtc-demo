b6740a900f23591442a02f9494e15cf82c679e93
Fernando De Vega <fernando.devega@admios-sa.com>
2020-06-07 20:46:17 -0500

    Simple Peer 2 Peer connection demo

---
+++ 02-simple-p2p-video/index.js
@@ -0,0 +1,49 @@
+navigator.getUserMedia({ video: true, audio: false}, (stream) => {
+
+  const Peer = require('simple-peer');
+
+  const peer = new Peer({
+    initiator: document.location.hash === '#init',
+    trickle: false,
+    stream
+  });
+
+  peer.on('signal', (data) => {
+    document.getElementById('caller').value = JSON.stringify(data);
+  })
+
+  document.getElementById('connect').addEventListener('click', () => {
+    const callee = JSON.parse(document.getElementById('callee').value);
+    peer.signal(callee);
+  });
+
+  document.getElementById('send').addEventListener('click', () => {
+    const message = document.getElementById('message').value;
+    document.getElementById('messages').appendChild(formatChats('You', message));
+    peer.send(message);
+  });
+
+  peer.on('data', (data) => {
+    document.getElementById('messages').appendChild(formatChats('Them', data));
+  });
+
+  peer.on('stream', function(stream) {
+    const cam = document.getElementById('cam');
+    cam.srcObject = stream;
+    cam.play();
+  });
+
+}, (err) => {
+  console.error(err);
+});
+
+
+function formatChats(who, text) {
+  const container = document.createElement('div');
+  const label = document.createElement('b');
+  label.append(`${who}: `);
+  container.appendChild(label);
+  container.append(text);
+
+  return container;
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webrtc-demo
 WebRTC Course Files
+
+These repo contains the code for the WebRTC classes we will be giving. I'll be adding to this little by little.
+
+Cheers,
+Fercho
+
+Challenge resources:
+https://andrei.codes/ascii-camera/
